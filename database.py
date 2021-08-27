c36232dde615cfa4f3749836da5859dee021e24e
Ukraine1449 <59920083+Ukraine1449@users.noreply.github.com>
2021-08-27 11:04:34 -0400

    Update database.py

--- database.py
+++ database.py
@@ -7,16 +7,26 @@ PASSWORD = "F5rFojAJrcgUU2VPriSI"
 db_connection = mysql.connect(host=HOST, database=DATABASE, user=USER, password=PASSWORD)
 print("Connected to:", db_connection.get_server_info())
 c = db_connection.cursor()
-c.execute('CREATE TABLE IF NOT EXISTS UserActivity(UUID BIGINT, TotalMessages int, UR int, XP int)')
+c.execute('CREATE TABLE IF NOT EXISTS UserActivity(UUID BIGINT, TotalMessages int, Level int, XP int)')
 
 
-def addMessage(UUID, oldMessageNum):
+def addMessage(UUID, oldMessageNum, oldXPNum):
     c.execute(f'UPDATE UserActivity SET TotalMessages="{oldMessageNum + 1}" WHERE UUID={UUID}')
+    db_connection.commit()
+    c.execute(f'UPDATE UserActivity SET XP="{oldXPNum + 1}" WHERE UUID={UUID}')
     db_connection.commit()
 
 
-def getTotal(UUID):
+def getTotalMessages(UUID):
     c.execute(f'SELECT TotalMessages FROM UserActivity where UUID ={UUID}')
+    print(c.fetchone().index(1))
+    #toreturn = c.fetchone().index(0)
+    db_connection.commit()
+    #return toreturn
+
+
+def getTotalXP(UUID):
+    c.execute(f'SELECT XP FROM UserActivity where UUID ={UUID}')
     toreturn = c.fetchone().index(0)
     db_connection.commit()
     return toreturn
@@ -26,6 +36,14 @@ def newUser(UUID):
     c.execute(f"INSERT INTO UserActivity(UUID) VALUES ({UUID})")
     db_connection.commit()
 
+
 def userLeave(UUID):
     c.execute(f'DELETE FROM UserActivity WHERE UUID={UUID}')
     db_connection.commit()
+
+
+def getLevel(UUID):
+    c.execute(f'SELECT XP FROM UserActivity where UUID ={UUID}')
+    toreturn = c.fetchone().index(0)
+    db_connection.commit()
+    return toreturn
